787c9dcbd815adca6cbd55ad87216a7a8143ff86
Johan Stenstam <johani@johani.org>
2024-02-14 18:00:40 +0100

    * tweaks to the API for parsing input text or CSV for the DAWG generator

--- cmd/debugcmd.go
+++ cmd/debugcmd.go
@@ -118,7 +118,6 @@ var debugSyncZoneCmd = &cobra.Command{
 		zd := tapir.ZoneData{
 			ZoneType:   3, // zonetype=3 keeps RRs in a []OwnerData, with an OwnerIndex map[string]int to locate stuff
 			ZoneName:   tapir.GlobalCF.Zone,
-			RRKeepFunc: func(uint16) bool { return true },
 			Logger:     log.Default(),
 		}
 
